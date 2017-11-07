d9dea34afcef3d136596cbc89517513426e7ffa9
Alexander Monaco <32403661+amona010@users.noreply.github.com>
2017-11-07 15:05:47 -0500

    Add files via upload

---
+++ Game.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewLab2
+{
+    public class Game
+    {
+        public String team1 { get; set; }
+
+        public int team1Score { get; set; }
+
+        public String team2 { get; set; }
+
+        public int team2Score { get; set; }
+
+        public Game()
+        {
+
+        }
+
+        public Game(String team1, int team1Score, String team2, int team2Score)
+        {
+            this.team1 = team1;
+            this.team2 = team2;
+            this.team1Score = team1Score;
+            this.team2Score = team2Score;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("{0} ({1}) - {2} ({3})", team1, team1Score, team2, team2Score);
+        }
+    }
+}
---
+++ GameFactory.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace NewLab2
+{
+    public class GameFactory
+    {
+        public List<Game> gameList;
+        StreamWriter writer;
+        XmlSerializer serial;
+
+
+        public String FilePath { get; set; }
+
+        public void createGameList()
+        {
+            gameList = new List<Game>();
+
+            gameList.Add(new Game("Flies", 12, "Beetles", 23));
+            gameList.Add(new Game("Flies", 53, "Beetles", 64));
+            gameList.Add(new Game("Flies", 43, "Beetles", 44));
+            gameList.Add(new Game("Flies", 64, "Beetles", 75));
+            gameList.Add(new Game("Flies", 33, "Beetles", 43));
+            gameList.Add(new Game("Flies", 90, "Beetles", 12));
+        }
+
+        public Boolean serializeGameList()
+        {
+            try
+            {
+                serial = new XmlSerializer(gameList.GetType());
+                writer = new StreamWriter(@"..\..\game.xml");
+                serial.Serialize(writer, gameList);
+                writer.Close();
+                return true;
+            }
+            catch(Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
